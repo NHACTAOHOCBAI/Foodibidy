783f36e718191a17be13503d3603f2bafb439ddd
phuc nguyen <dangphucnguyen20112005@gmail.com>
2025-05-04 17:35:02 +0700

    chore: setup with nativewind

---
+++ FE/app/_layout.tsx
@@ -0,0 +1,10 @@
+import { Stack } from "expo-router";
+import './global.css'
+import { StatusBar } from "react-native";
+export default function RootLayout() {
+  return (
+    <>
+      <Stack />
+    </>
+  )
+}
