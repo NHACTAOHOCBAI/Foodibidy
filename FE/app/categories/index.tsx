c02e60954a3a9c9638968ed9447351e98154d3c4
phuc nguyen <dangphucnguyen20112005@gmail.com>
2025-05-23 22:01:52 +0700

    feat: finish see all cate

--- FE/app/categories/index.tsx
+++ FE/app/categories/index.tsx
@@ -2,23 +2,21 @@ import { Text } from 'react-native';
 import { getCategoriesPaginated } from '@/services/mockAPI';
 import CategoryItem from '@/app/categories/CategoryItem';
 import LazyFlatList from '@/components/LazyFlatList';
+import { getCategory } from '@/services/category';
 
 
 const PAGE_SIZE = 6;
 
 const AllCategories = () => {
     const fetchCategoris = async (page: number) => {
-        return await getCategoriesPaginated({
-            page,
-            limit: PAGE_SIZE,
-        });
+        return await getCategory(page, PAGE_SIZE);
     };
     const renderHeader = () => (
         <Text className="text-[#32343E] text-[20px] mb-4 pt-[100px] px-[24px]">Popular Categories</Text>
     );
 
     return (
-        <LazyFlatList
+        <LazyFlatList<Category>
             fetchData={fetchCategoris}
             pageSize={PAGE_SIZE}
             renderItem={({ item }) => <CategoryItem category={item} />}
