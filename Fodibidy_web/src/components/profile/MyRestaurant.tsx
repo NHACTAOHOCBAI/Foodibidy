2eef2963ce622815b7ee4c14f953607857a22d81
phuc nguyen <dangphucnguyen20112005@gmail.com>
2025-06-29 21:03:31 +0700

    fix: fix my profile

--- Fodibidy_web/src/components/profile/MyRestaurant.tsx
+++ Fodibidy_web/src/components/profile/MyRestaurant.tsx
@@ -1,23 +1,21 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { Form, Input, Button, Select, message } from "antd";
+import { Form, Input, Button, Select, message, Spin } from "antd";
 import { useEffect, useState } from "react";
 import type { UploadFile } from "antd/es/upload/interface";
 import TextArea from "antd/es/input/TextArea";
 import UploadImage from "../UploadImage";
 import { MdOutlineStar } from "react-icons/md";
 import convertDateFormat from "../../utils/convertDateFormat";
-import { updateMyRestaurant } from "../../services/auth";
+import { getMyProfile, updateMyRestaurant } from "../../services/auth";
 
-interface MyRestaurantProps {
-    myRestaurant: any;
-    fetchMyProfile: () => Promise<void>
-}
 
-const MyRestaurant = ({ myRestaurant, fetchMyProfile }: MyRestaurantProps) => {
+const MyRestaurant = () => {
+    const [myProfile, setMyProfile] = useState<Restaurant | null>(null);
     const [form] = Form.useForm();
     const [messageApi, contextHolder] = message.useMessage();
     const [isPending, setIsPending] = useState(false)
+    const [isLoading, setIsLoading] = useState(false)
     const [fileList, setFileList] = useState<UploadFile[]>([]);
     const onFinish = async (values: any) => {
         setIsPending(true)
@@ -39,117 +37,133 @@ const MyRestaurant = ({ myRestaurant, fetchMyProfile }: MyRestaurantProps) => {
     };
     const handleReset = () => {
         form.setFieldsValue({
-            restaurantName: myRestaurant?.restaurantName,
-            address: myRestaurant?.address,
-            status: myRestaurant?.status,
-            phoneNumber: myRestaurant?.phoneNumber,
-            bio: myRestaurant?.bio
+            restaurantName: myProfile?.restaurantName,
+            address: myProfile?.address,
+            status: myProfile?.status,
+            phoneNumber: myProfile?.phoneNumber,
+            bio: myProfile?.bio
         })
         setFileList([
             {
                 uid: '-1',
                 name: 'avatar.jpg',
                 status: 'done',
-                url: myRestaurant?.avatar
+                url: myProfile?.restaurantImage
             },
         ])
     }
-    useEffect(() => {
+    const fetchMyProfile = async () => {
+        setIsLoading(true)
+        const res = await getMyProfile()
         form.setFieldsValue({
-            restaurantName: myRestaurant?.restaurantName,
-            address: myRestaurant?.address,
-            status: myRestaurant?.status,
-            phoneNumber: myRestaurant?.phoneNumber,
-            bio: myRestaurant?.bio
+            restaurantName: res.restaurant.restaurantName,
+            address: res.restaurant.address,
+            status: res.restaurant.status,
+            phoneNumber: res.restaurant.phoneNumber,
+            bio: res.restaurant.bio
         })
         setFileList([
             {
                 uid: '-1',
                 name: 'avatar.jpg',
                 status: 'done',
-                url: myRestaurant?.avatar
+                url: res.restaurant.restaurantImage
             },
         ])
-    }, [myRestaurant, form])
+
+        setMyProfile(res.restaurant)
+        setIsLoading(false)
+    }
+    useEffect(() => {
+        fetchMyProfile()
+    }, [])
     return (
-        <>
-            {contextHolder}
-            <Form
-                form={form}
-                layout="vertical"
-                name="basic"
-                labelCol={{ span: 26 }}
-                wrapperCol={{ span: 42 }}
-                style={{ maxWidth: 1200 }}
-                initialValues={{ remember: true }}
-                onFinish={onFinish}
-                autoComplete="off"
-            >
-                <div style={{ display: 'flex', gap: 20 }}>
-                    <div>
-                        <Form.Item
-                            label="Restaurant Name"
-                            name="restaurantName"
-                            rules={[{ required: true, message: 'Enter restaurant name!' }]}
-                        >
-                            <Input disabled={isPending} style={{ width: 400 }} />
-                        </Form.Item>
-                        <Form.Item
-                            label="Address"
-                            name="address"
-                            rules={[{ required: true, message: 'Enter restaurant address!' }]}
-                        >
-                            <Input disabled={isPending} />
-                        </Form.Item>
-                        <Form.Item
-                            label="Status"
-                            name="status"
-                            rules={[{ required: true, message: 'Enter restaurant status!' }]}
-                        >
-                            <Select disabled={isPending} />
-                        </Form.Item>
-                        <Form.Item
-                            label="Phone"
-                            name="phoneNumber"
-                            rules={[{ required: true, message: 'Enter restaurant phone!' }]}
-                        >
-                            <Input disabled={isPending} />
-                        </Form.Item>
-                        <Form.Item
-                            label="Bio"
-                            name="bio"
-                            rules={[{ required: true, message: 'Enter restaurant bio!' }]}
-                        >
-                            <TextArea
-                                disabled={isPending}
-                                rows={3}
-                            />
-                        </Form.Item>
-                    </div>
-                    <div>
-                        <Form.Item label="Image">
-                            <div style={{ border: '1px dashed #d9d9d9', padding: 8, borderRadius: 4, width: 400 }}>
-                                <UploadImage
-                                    isPending={isPending}
-                                    fileList={fileList}
-                                    setFileList={setFileList}
+        isLoading ?
+            <div style={{ width: '100%', height: '100%', display: "flex", justifyContent: 'center', alignItems: 'center' }}> <Spin size="large" /> </div>
+            :
+            <>
+                {contextHolder}
+                <Form
+                    form={form}
+                    layout="vertical"
+                    name="basic"
+                    labelCol={{ span: 26 }}
+                    wrapperCol={{ span: 42 }}
+                    style={{ maxWidth: 1200 }}
+                    initialValues={{ remember: true }}
+                    onFinish={onFinish}
+                    autoComplete="off"
+                >
+                    <div style={{ display: 'flex', gap: 20 }}>
+                        <div>
+                            <Form.Item
+                                label="Restaurant Name"
+                                name="restaurantName"
+                                rules={[{ required: true, message: 'Enter restaurant name!' }]}
+                            >
+                                <Input disabled={isPending} style={{ width: 400 }} />
+                            </Form.Item>
+                            <Form.Item
+                                label="Address"
+                                name="address"
+                                rules={[{ required: true, message: 'Enter restaurant address!' }]}
+                            >
+                                <Input disabled={isPending} />
+                            </Form.Item>
+                            <Form.Item
+                                label="Status"
+                                name="status"
+                                rules={[{ required: true, message: 'Enter restaurant status!' }]}
+                            >
+                                <Select
+                                    options={[
+                                        { text: "active", value: "active" },
+                                        { text: "inactive", value: "inactive" }
+                                    ]}
+                                    disabled={isPending} />
+                            </Form.Item>
+                            <Form.Item
+                                label="Phone"
+                                name="phoneNumber"
+                                rules={[{ required: true, message: 'Enter restaurant phone!' }]}
+                            >
+                                <Input disabled={isPending} />
+                            </Form.Item>
+                            <Form.Item
+                                label="Bio"
+                                name="bio"
+                                rules={[{ required: true, message: 'Enter restaurant bio!' }]}
+                            >
+                                <TextArea
+                                    disabled={isPending}
+                                    rows={3}
                                 />
-                            </div>
-                        </Form.Item>
-                        <p>Sold item : {myRestaurant?.purchase}</p>
-                        <div style={{ display: "flex", alignItems: 'center', gap: 10 }}>
-                            Rating : {myRestaurant?.rating} <MdOutlineStar size={20} style={{ color: "yellow" }} />
+                            </Form.Item>
                         </div>
-                        <p>Created at : {convertDateFormat(myRestaurant?.createdAt)}</p>
-                        <p>Owner : {myRestaurant?.user.fullName} ({myRestaurant?.user.phoneNumber})</p>
+                        <div>
+                            <Form.Item label="Image">
+                                <div style={{ border: '1px dashed #d9d9d9', padding: 8, borderRadius: 4, width: 400 }}>
+                                    <UploadImage
+                                        isPending={isPending}
+                                        fileList={fileList}
+                                        setFileList={setFileList}
+                                    />
+                                </div>
+                            </Form.Item>
+                            <p>Sold item : {myProfile?.purchase}</p>
+                            <div style={{ display: "flex", alignItems: 'center', gap: 10 }}>
+                                Rating : {myProfile?.rating} <MdOutlineStar size={20} style={{ color: "yellow" }} />
+                            </div>
+                            <p>Created at : {convertDateFormat(myProfile?.createdAt || "")}</p>
+                            <p>Owner : {myProfile?.user?.fullName} ({myProfile?.user?.phoneNumber})</p>
+                        </div>
                     </div>
-                </div>
-                <div style={{ display: 'flex', gap: 20 }}>
-                    <Button disabled={isPending} onClick={() => handleReset()}>Reset</Button>
-                    <Button loading={isPending} type="primary" onClick={() => form.submit()}>Save</Button>
-                </div>
-            </Form>
-        </>
+                    <div style={{ display: 'flex', gap: 20 }}>
+                        <Button disabled={isPending} onClick={() => handleReset()}>Reset</Button>
+                        <Button loading={isPending} type="primary" onClick={() => form.submit()}>Save</Button>
+                    </div>
+                </Form>
+            </>
     );
 }
 
--- Fodibidy_web/src/pages/admin/Profile.tsx
+++ Fodibidy_web/src/pages/admin/Profile.tsx
@@ -1,34 +1,18 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { Spin, Tabs } from 'antd';
+import { Tabs } from 'antd';
 import type { TabsProps } from 'antd';
 import MyRestaurant from '../../components/profile/MyRestaurant';
 import MyProfile from '../../components/profile/MyProfile';
-import { useContext, useEffect, useState } from 'react';
-import { getMyProfile } from '../../services/auth';
+import { useContext } from 'react';
 import { MyProfileContext } from '../../context/MyProfileContext';
 
 const onChange = (key: string) => {
     console.log(key);
 };
 
-type MyProfileType = {
-    restaurant?: any;
-    user: any
-};
 
 const Profile = () => {
     const { myProfile: profile } = useContext(MyProfileContext)
-    const [myProfile, setMyProfile] = useState<MyProfileType | undefined>(undefined)
-    const [isPending, setIsPending] = useState(false)
-    const fetchMyProfile = async () => {
-        setIsPending(true)
-        const res = await getMyProfile()
-        setMyProfile(res)
-        setIsPending(false)
-    }
-    useEffect(() => {
-        fetchMyProfile()
-    }, [])
     const items: TabsProps['items'] = [
         ...(profile?.role === 'restaurant'
             ? [
@@ -36,8 +20,7 @@ const Profile = () => {
                     key: '1',
                     label: 'Restaurant',
                     children: <MyRestaurant
-                        fetchMyProfile={fetchMyProfile}
-                        myRestaurant={myProfile?.restaurant} />
+                    />
                 },
             ]
             : []),
@@ -45,16 +28,12 @@ const Profile = () => {
             key: '2',
             label: 'Account',
             children: <MyProfile
-                fetchMyProfile={fetchMyProfile}
-                myAccount={myProfile?.user}
+
             />,
         },
     ];
     return (
-        isPending ?
-            <div style={{ width: '100%', height: '100%', display: "flex", justifyContent: 'center', alignItems: 'center' }}> <Spin size="large" /> </div>
-            :
-            <Tabs defaultActiveKey="1" items={items} onChange={onChange} />
+        <Tabs defaultActiveKey="1" items={items} onChange={onChange} />
     )
 }
 
