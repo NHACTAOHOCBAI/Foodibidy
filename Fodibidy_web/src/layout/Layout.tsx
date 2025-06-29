23827bfdccd50885f2144989c111514ecd136b38
phuc nguyen <dangphucnguyen20112005@gmail.com>
2025-06-29 14:52:10 +0700

    feat : finish logout

--- Fodibidy_web/src/layout/Layout.tsx
+++ Fodibidy_web/src/layout/Layout.tsx
@@ -1,10 +1,11 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import React, { useContext, useEffect, useState } from 'react';
 import {
     MenuFoldOutlined,
     MenuUnfoldOutlined,
     UserOutlined,
 } from '@ant-design/icons';
-import { Avatar, Button, Layout, Menu, Popover, theme } from 'antd';
+import { Avatar, Button, Layout, Menu, message, Popover, Spin, theme } from 'antd';
 import { Link, Outlet, useLocation, useNavigate } from 'react-router';
 import { RxDashboard } from 'react-icons/rx';
 import { PiBowlFood } from 'react-icons/pi';
@@ -13,10 +14,13 @@ import { SiHomeassistantcommunitystore } from 'react-icons/si';
 import { MdOutlineAccountCircle, MdOutlineFoodBank } from 'react-icons/md';
 import { ImProfile } from 'react-icons/im';
 import { MyProfileContext } from '../context/MyProfileContext';
+import { logout } from '../services/auth';
 
 const { Header, Sider, Content } = Layout;
 
 const AppLayout: React.FC = () => {
+    const [messageApi, contextHolder] = message.useMessage();
+    const [isPending, setIsPending] = useState(false)
     const navigate = useNavigate()
     const [collapsed, setCollapsed] = useState(false);
     const {
@@ -86,6 +90,20 @@ const AppLayout: React.FC = () => {
             },
         ]
     }
+    const handleLogout = async () => {
+        setIsPending(true)
+        try {
+            await logout();
+            localStorage.removeItem("accessToken")
+            localStorage.removeItem("profile")
+            navigate('/login')
+            messageApi.success("Logout successfully")
+        }
+        catch (err: any) {
+            messageApi.error(err.error)
+        }
+        setIsPending(false)
+    }
     useEffect(() => {
         const profile = localStorage.getItem("profile")
         if (profile) {
@@ -94,64 +112,76 @@ const AppLayout: React.FC = () => {
         }
     }, [])
     return (
-        <Layout style={{ height: '100%' }}>
-            <Sider trigger={null} collapsible collapsed={collapsed}>
-                <div className="demo-logo-vertical" />
-                <Menu
-                    selectedKeys={[endpoints]}
-                    theme="dark"
-                    mode="inline"
-                    items={items}
-                />
-            </Sider>
-            <Layout>
-                <Header style={{ padding: 0, background: colorBgContainer }}>
-                    <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
-                        <Button
-                            type="text"
-                            icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
-                            onClick={() => setCollapsed(!collapsed)}
-                            style={{
-                                fontSize: '16px',
-                                width: 64,
-                                height: 64,
-                            }}
-                        />
-                        <Popover content={
-                            <div >
-                                <Button
-                                    style={{ display: 'block', width: 150, marginBottom: 10 }}
-                                    onClick={() => navigate('/profile')}
-                                >View profile</Button>
-                                <Button style={{ display: 'block', width: 150 }}>Log out</Button>
-                            </div>
-                        } >
-                            <div style={{ display: 'flex', alignItems: 'center', gap: 10, marginRight: 20 }}>
-                                {
-                                    myProfile?.avatar === "" ?
-                                        <Avatar style={{ backgroundColor: '#87d068' }} icon={<UserOutlined />} />
-                                        :
-                                        <Avatar src={<img src={myProfile?.avatar} alt="avatar" />} />
-                                }
-                                <div >{myProfile?.fullName}</div>
-                            </div>
-                        </Popover>
+        <>
+            {
+                isPending ?
+                    <div style={{ width: '100%', height: '100%', display: "flex", justifyContent: 'center', alignItems: 'center' }}> <Spin size="large" /> </div>
+                    :
+                    <>
+                        {contextHolder}
+                        <Layout style={{ height: '100%' }}>
+                            <Sider trigger={null} collapsible collapsed={collapsed}>
+                                <div className="demo-logo-vertical" />
+                                <Menu
+                                    selectedKeys={[endpoints]}
+                                    theme="dark"
+                                    mode="inline"
+                                    items={items}
+                                />
+                            </Sider>
+                            <Layout>
+                                <Header style={{ padding: 0, background: colorBgContainer }}>
+                                    <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+                                        <Button
+                                            type="text"
+                                            icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
+                                            onClick={() => setCollapsed(!collapsed)}
+                                            style={{
+                                                fontSize: '16px',
+                                                width: 64,
+                                                height: 64,
+                                            }}
+                                        />
+                                        <Popover content={
+                                            <div >
+                                                <Button
+                                                    style={{ display: 'block', width: 150, marginBottom: 10 }}
+                                                    onClick={() => navigate('/profile')}
+                                                >View profile</Button>
+                                                <Button style={{ display: 'block', width: 150 }} onClick={() => handleLogout()}>Log out</Button>
+                                            </div>
+                                        } >
+                                            <div style={{ display: 'flex', alignItems: 'center', gap: 10, marginRight: 20 }}>
+                                                {
+                                                    myProfile?.avatar === "" ?
+                                                        <Avatar style={{ backgroundColor: '#87d068' }} icon={<UserOutlined />} />
+                                                        :
+                                                        <Avatar src={<img src={myProfile?.avatar} alt="avatar" />} />
+                                                }
+                                                <div >{myProfile?.fullName}</div>
+                                            </div>
+                                        </Popover>
 
-                    </div>
-                </Header>
-                <Content
-                    style={{
-                        margin: '24px 16px',
-                        padding: 24,
-                        minHeight: 280,
-                        background: colorBgContainer,
-                        borderRadius: borderRadiusLG,
-                    }}
-                >
-                    <Outlet />
-                </Content>
-            </Layout>
-        </Layout>
+                                    </div>
+                                </Header>
+                                <Content
+                                    style={{
+                                        margin: '24px 16px',
+                                        padding: 24,
+                                        minHeight: 280,
+                                        background: colorBgContainer,
+                                        borderRadius: borderRadiusLG,
+                                    }}
+                                >
+                                    <Outlet />
+                                </Content>
+                            </Layout>
+                        </Layout>
+                    </>
+
+            }
+
+        </>
     );
 };
 
